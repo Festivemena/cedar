fea788ff41a0b85f8a1aaedfe187b0c2594a8720
Festivemena <88979259+Festivemena@users.noreply.github.com>
2023-01-11 19:40:08 +0100

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,7 @@ import type { AppProps } from 'next/app';
 import { useEffect, useState } from 'react';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-import Header from '../components/Header';
-import Sidebar from '../components/Sidebar';
+
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
@@ -16,18 +15,9 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   if (isSSR) return null;
 
   return (
-    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
-      <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
-        <Header />
-        <div className='flex gap-6 md:gap-2 '>
-          <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
-            <Sidebar />
-          </div>
-          <div className='mt-4 flex flex-col gap-6 overflow-auto h-[88vh] videos flex-1'>
-            <Component {...pageProps} />
-          </div>
-        </div>
-      </div>
+    <GoogleOAuthProvider clientId={`993638040659-2m136lrh4cf02eh2770cd8nlvph4mg8u.apps.googleusercontent.com`}>
+      <Component {...pageProps} />
+
     </GoogleOAuthProvider>
   );
 };
